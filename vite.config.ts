d79f489614596ea568e299b827dbda588afdb631
peswes <ayomikunleadejuwon@gmail.com>
2024-11-21 14:01:36 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/west_chatup/',
+  base: '/west_chatup/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
